d2a88ff1be09616a903706cb22196b65c57452a6
Angela <angieadrigarcia572@gmail.com>
2026-02-22 11:09:50 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,43 +1,13 @@
 let express = require('express');
 let app = express();
 
+// 1. Esto ya lo tienes:
 console.log("Hello World");
 
+// 2. NUEVO: Responder a las peticiones GET en la raíz "/"
 app.get("/", function(req, res) {
   res.send("Hello Express");
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+// No olvides exportar app si el boilerplate lo requiere
+module.exports = app;
